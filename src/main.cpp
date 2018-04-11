ed2cb4c3d585de9a3ca0423c4e01ba829baa03b2
edyuto <edson.toma@usp.br>
2018-04-11 17:35:31 -0300

    ident

--- src/main.cpp
+++ src/main.cpp
@@ -5,80 +5,80 @@
 const GLint WINDOWS_WIDTH = 800, WINDOWS_HEIGHT = 600;
 
 void draw_circle(spider::circle *circle){
-    int n_lines = 100;
-    GLdouble x,y;
+   int n_lines = 100;
+   GLdouble x,y;
 
-    x = circle->center.x + circle->radius;
-    y = circle->center.y;
-    glBegin(GL_TRIANGLE_STRIP);
-    for(int i = 0; i <= n_lines; i++){
-        glVertex2d(circle->center.x, circle->center.y);
-        glVertex2d(x, y);
-        x = circle->center.x + circle->radius * cos(2*M_PI*i/n_lines);
-        y = circle->center.y + circle->radius * sin(2*M_PI*i/n_lines);
-        glVertex2d(x,y);
-    }
-    glEnd();
-    glFlush();
+   x = circle->center.x + circle->radius;
+   y = circle->center.y;
+   glBegin(GL_TRIANGLE_STRIP);
+   for(int i = 0; i <= n_lines; i++){
+      glVertex2d(circle->center.x, circle->center.y);
+      glVertex2d(x, y);
+      x = circle->center.x + circle->radius * cos(2*M_PI*i/n_lines);
+      y = circle->center.y + circle->radius * sin(2*M_PI*i/n_lines);
+      glVertex2d(x,y);
+   }
+   glEnd();
+   glFlush();
 }
 
 void draw_leg(spider::leg *leg){
-    glLineWidth(5);
-    glBegin(GL_LINE_STRIP);
-    glVertex2d(leg->orig.x, leg->orig.y);
-    glVertex2d(leg->articulation.x, leg->articulation.y);
-    glVertex2d(leg->end.x, leg->end.y);
-    glEnd();
-    glFlush();
+   glLineWidth(5);
+   glBegin(GL_LINE_STRIP);
+   glVertex2d(leg->orig.x, leg->orig.y);
+   glVertex2d(leg->articulation.x, leg->articulation.y);
+   glVertex2d(leg->end.x, leg->end.y);
+   glEnd();
+   glFlush();
 
 }
 
 void draw_spider(spider *s){
-    glColor3f(0, 0, 0);
-    draw_circle(s->cephalothorax);
-    draw_circle(s->abdomen);
-    for(int i = 0; i < 8; i++)
-        draw_leg(s->legs[i]);
+   glColor3f(0, 0, 0);
+   draw_circle(s->cephalothorax);
+   draw_circle(s->abdomen);
+   for(int i = 0; i < 8; i++)
+      draw_leg(s->legs[i]);
 
-    glColor3f(1,0,0);
-    draw_circle(s->eyes[0]);
-    draw_circle(s->eyes[1]);
+   glColor3f(1,0,0);
+   draw_circle(s->eyes[0]);
+   draw_circle(s->eyes[1]);
 }
 
 void init(){
-    glClearColor(1, 1, 1, 1);
-    glMatrixMode(GL_PROJECTION);
+   glClearColor(1, 1, 1, 1);
+   glMatrixMode(GL_PROJECTION);
 
-    gluOrtho2D(0, WINDOWS_WIDTH, 0, WINDOWS_HEIGHT);
+   gluOrtho2D(0, WINDOWS_WIDTH, 0, WINDOWS_HEIGHT);
 }
 
 void callback_display(){
-    spider::t_point *p = new spider::t_point;
-    p->x = WINDOWS_WIDTH/2;
-    p->y = WINDOWS_HEIGHT/2;
+   spider::t_point *p = new spider::t_point;
+   p->x = WINDOWS_WIDTH/2;
+   p->y = WINDOWS_HEIGHT/2;
 
-    spider *s = new spider(p);
-    glClear(GL_COLOR_BUFFER_BIT);
-    draw_spider(s);
-    glutSwapBuffers();
+   spider *s = new spider(p);
+   glClear(GL_COLOR_BUFFER_BIT);
+   draw_spider(s);
+   glutSwapBuffers();
 
 }
 
 int main(int argc, char *argv[]){
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+   glutInit(&argc, argv);
+   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 
-    GLint screen_width = glutGet(GLUT_SCREEN_WIDTH),
-            screen_height = glutGet(GLUT_SCREEN_HEIGHT);
+   GLint screen_width = glutGet(GLUT_SCREEN_WIDTH),
+         screen_height = glutGet(GLUT_SCREEN_HEIGHT);
 
-    glutInitWindowPosition((screen_width - WINDOWS_WIDTH)/2, (screen_height - WINDOWS_HEIGHT)/2);
-    glutInitWindowSize(WINDOWS_WIDTH, WINDOWS_HEIGHT);
-    glutCreateWindow("Aranha 2D");
+   glutInitWindowPosition((screen_width - WINDOWS_WIDTH)/2, (screen_height - WINDOWS_HEIGHT)/2);
+   glutInitWindowSize(WINDOWS_WIDTH, WINDOWS_HEIGHT);
+   glutCreateWindow("Aranha 2D");
 
-    init();
-    glutDisplayFunc(callback_display);
+   init();
+   glutDisplayFunc(callback_display);
 
-    glutMainLoop();
+   glutMainLoop();
 
-    return 0;
+   return 0;
 }
--- src/spider.cpp
+++ src/spider.cpp
@@ -12,67 +12,66 @@
 #define ARTICANG 15
 
 spider::spider(spider::t_point *pos) {
-    this->abdomen = new circle;
-    this->cephalothorax = new circle;
-    this->legs = new leg*[8];
-    this->eyes = new circle*[2];
-    this->center = pos;
+   this->abdomen = new circle;
+   this->cephalothorax = new circle;
+   this->legs = new leg*[8];
+   this->eyes = new circle*[2];
+   this->center = pos;
 
-    this->abdomen->center.y = pos->y - TORAXSIZE + TORAXSIZE/10;
-    this->abdomen->center.x = pos->x;
-    this->abdomen->radius = TORAXSIZE;
+   this->abdomen->center.y = pos->y - TORAXSIZE + TORAXSIZE/10;
+   this->abdomen->center.x = pos->x;
+   this->abdomen->radius = TORAXSIZE;
 
-    this->cephalothorax->center.y = pos->y + TORAXSIZE/2;
-    this->cephalothorax->center.x = pos->x;
-    this->cephalothorax->radius = TORAXSIZE/2;
+   this->cephalothorax->center.y = pos->y + TORAXSIZE/2;
+   this->cephalothorax->center.x = pos->x;
+   this->cephalothorax->radius = TORAXSIZE/2;
 
-    this->eyes[0] = new circle;
-    this->eyes[0]->center.y = pos->y + TORAXSIZE - EYESIZE;
-    this->eyes[0]->center.x = pos->x - EYESIZE;
-    this->eyes[0]->radius = EYESIZE;
+   this->eyes[0] = new circle;
+   this->eyes[0]->center.y = pos->y + TORAXSIZE - EYESIZE;
+   this->eyes[0]->center.x = pos->x - EYESIZE;
+   this->eyes[0]->radius = EYESIZE;
 
-    this->eyes[1] = new circle;
-    this->eyes[1]->center.y = pos->y + TORAXSIZE - EYESIZE;
-    this->eyes[1]->center.x = pos->x + EYESIZE;
-    this->eyes[1]->radius = EYESIZE;
+   this->eyes[1] = new circle;
+   this->eyes[1]->center.y = pos->y + TORAXSIZE - EYESIZE;
+   this->eyes[1]->center.x = pos->x + EYESIZE;
+   this->eyes[1]->radius = EYESIZE;
 
-    int k = 0;
-    for(int i = 0; i < 8; i++){
-        this->legs[i] = new leg;
-        while((k >= 2 && k <= 4) || k == 9)
-            k++;
+   int k = 0;
+   for(int i = 0; i < 8; i++){
+      this->legs[i] = new leg;
+      while((k >= 2 && k <= 4) || k == 9)
+         k++;
 
-        this->legs[i]->orig.x = this->cephalothorax->center.x
-                               + cos(k*M_PI/6)
-                                 * this->cephalothorax->radius;
+      this->legs[i]->orig.x = this->cephalothorax->center.x
+         + cos(k*M_PI/6)
+         * this->cephalothorax->radius;
 
-        this->legs[i]->orig.y = this->cephalothorax->center.y
-                               + sin(k*M_PI/6)
-                                 * this->cephalothorax->radius;
+      this->legs[i]->orig.y = this->cephalothorax->center.y
+         + sin(k*M_PI/6)
+         * this->cephalothorax->radius;
 
-        if(k < 3 || k > 9) {
-            this->legs[i]->end.x = this->legs[i]->orig.x + LEGSIZE;
-            this->legs[i]->articulation.x = this->legs[i]->orig.x + LEGSIZE/2;
-        }
-        else{
-            this->legs[i]->end.x = this->legs[i]->orig.x - LEGSIZE;
-            this->legs[i]->articulation.x = this->legs[i]->orig.x - LEGSIZE/2;
-        }
+      if(k < 3 || k > 9) {
+         this->legs[i]->end.x = this->legs[i]->orig.x + LEGSIZE;
+         this->legs[i]->articulation.x = this->legs[i]->orig.x + LEGSIZE/2;
+      }
+      else{
+         this->legs[i]->end.x = this->legs[i]->orig.x - LEGSIZE;
+         this->legs[i]->articulation.x = this->legs[i]->orig.x - LEGSIZE/2;
+      }
 
-        this->legs[i]->end.y = this->legs[i]->orig.y - LEGSIZE/2;
-        this->legs[i]->articulation.y = this->legs[i]->orig.y + ARTICANG;
-
-
-        if(k == 1 || k == 5) {
-            this->legs[i]->articulation.y = this->legs[i]->orig.y + 3 * LEGSIZE / 2;
-            this->legs[i]->end.y = this->legs[i]->articulation.y + ARTICANG;
-        }
-
-        if(k == 8 || k == 10)
-            this->legs[i]->end.y = this->legs[i]->orig.y - 3*LEGSIZE/2;
+      this->legs[i]->end.y = this->legs[i]->orig.y - LEGSIZE/2;
+      this->legs[i]->articulation.y = this->legs[i]->orig.y + ARTICANG;
 
 
-        k++;
-    }
+      if(k == 1 || k == 5) {
+         this->legs[i]->articulation.y = this->legs[i]->orig.y + 3 * LEGSIZE / 2;
+         this->legs[i]->end.y = this->legs[i]->articulation.y + ARTICANG;
+      }
 
+      if(k == 8 || k == 10)
+         this->legs[i]->end.y = this->legs[i]->orig.y - 3*LEGSIZE/2;
+
+
+      k++;
+   }
 }
